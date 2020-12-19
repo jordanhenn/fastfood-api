6bf0c14c6fd1f745d18db4b006685d39f8518a5f
Jordan Henn <jordanwalkerhenn@gmail.com>
2020-12-19 15:59:46 -0800

    fixed items & creations table

--- test/setup.js
+++ test/setup.js
@@ -1,4 +1,16 @@
-const { expect } = require('chai')
+process.env.TZ = 'UCT'
+process.env.NODE_ENV = 'test'
+
+require('dotenv').config()
+
+process.env.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL
+  || "postgresql://dunder_mifflin@localhost/fastfood-test"
+
+const deepEqualInAnyOrder = require('deep-equal-in-any-order')
+const chai = require('chai')
+
+chai.use(deepEqualInAnyOrder)
+const { expect } = chai
 const supertest = require('supertest')
 
 global.expect = expect
---
+++ test/test-helpers.js
@@ -0,0 +1,580 @@
+function makeBunsArray() {
+    return [
+        {   
+            id: 1,
+            bun_name: 'innout_standard',
+            bun_description: 'In N Out'
+        },
+        {   
+            id: 2,
+            bun_name: 'burgerking_sesame',
+            bun_description: 'Burger King'
+        },
+        {   
+            id: 3,
+            bun_name: 'carlsjr_standard',
+            bun_description: 'Carls Jr'
+        },
+        {   
+            id: 4,
+            bun_name: 'jack_baconultimate',
+            bun_description: 'Jack in the Box Standard'
+        },
+        {   
+            id: 5,
+            bun_name: 'mcdonalds_artisan',
+            bun_description: 'McDonalds Artisan'
+        },
+        {   
+            id: 6,
+            bun_name: 'jack_sourdough',
+            bun_description: 'Jack in the Box Sourdough'
+        },
+        {   
+            id: 7,
+            bun_name: 'mcdonalds_mcgriddle',
+            bun_description: 'McDonalds McGriddle'
+        },
+        {   
+            id: 8,
+            bun_name: 'mcdonalds_mcmuffin',
+            bun_description: 'McDonalds McMuffin'
+        },
+        {   
+            id: 9,
+            bun_name: 'jack_butteryjack',
+            bun_description: 'Jack in the Box Buttery Jack'
+        },
+        {   
+            id: 10,
+            bun_name: 'mcdonalds_sesame',
+            bun_description: 'McDonalds Sesame'
+        },
+        {   
+            id: 11,
+            bun_name: 'mcdonalds_standard',
+            bun_description: 'McDonalds Standard'
+        },
+        {   
+            id: 12,
+            bun_name: 'wendys_standard',
+            bun_description: 'Wendys'
+        }
+    ]
+  }
+
+  function makeSaucesArray() {
+    return [
+        {
+            id: 1,
+            sauce_name: 'innout_spread',
+            sauce_description: 'In n Out Spread',
+        },
+        {
+            id: 2,
+            sauce_name: 'mcdonalds_bigmacsauce',
+            sauce_description: 'McDonalds Big Mac Sauce',
+        },
+        {
+            id: 3,
+            sauce_name: 'carlsjr_bigtwinsauce',
+            sauce_description: 'Carls Jr Big Twin Sauce',
+        },
+        {
+            id: 4,
+            sauce_name: 'burgerking_whoppersauce',
+            sauce_description: 'Burger King Whopper Sauce',
+        },
+        {
+            id: 5,
+            sauce_name: 'jack_secretsauce',
+            sauce_description: 'Jack in the Box Secret Sauce',
+        },
+        {
+            id: 6,
+            sauce_name: 'jack_tacosauce',
+            sauce_description: 'Jack in the Box Taco Sauce',
+        },
+        {
+            id: 7,
+            sauce_name: 'wendys_sawesomesauce',
+            sauce_description: 'Wendys Sawesome Sauce',
+        }
+    ]
+  }
+
+  function makeFillingsArray() {
+    return [
+        {
+            id: 1,
+            filling_name: 'burgerking_baconking',
+            filling_description: 'Burger King Bacon King',
+        },
+        {
+            id: 2,
+            filling_name: 'burgerking_chickenfries',
+            filling_description: 'Burger King Chicken Fries',
+        },
+        {
+            id: 3,
+            filling_name: 'burgerking_doublecheeseburger',
+            filling_description: 'Burger King Double Cheeseburger',
+        },
+        {
+            id: 4,
+            filling_name: 'burgerking_hersheyspie',
+            filling_description: 'Burger King Hersheys Sundae Pie',
+        },
+        {
+            id: 5,
+            filling_name: 'burgerking_whopper',
+            filling_description: 'Burger King Whopper',
+        },
+        {
+            id: 6,
+            filling_name: 'carlsjr_doublewesternbacon',
+            filling_description: 'Carls Jr Double Western Bacon',
+        },
+        {
+            id: 7,
+            filling_name: 'carlsjr_jalapenopopper',
+            filling_description: 'Carls Jr Jalapeno Poppers',
+        },
+        {
+            id: 8,
+            filling_name: 'carlsjr_superstar',
+            filling_description: 'Carls Jr Super Star',
+        },
+        {
+            id: 9,
+            filling_name: 'innout_animalstylefries',
+            filling_description: 'In N Out Animal Style Fries',
+        },
+        {
+            id: 10,
+            filling_name: 'innout_doubledouble',
+            filling_description: 'In N Out Double Double',
+        },
+        {
+            id: 11,
+            filling_name: 'innout_neopolitan',
+            filling_description: 'In N Out Neopolitan Shake',
+        },
+        {
+            id: 12,
+            filling_name: 'jack_baconswissbuttery',
+            filling_description: 'Jack in the Box Bacon & Swiss Butter Jack',
+        },
+        {
+            id: 13,
+            filling_name: 'jack_curlyfries',
+            filling_description: 'Jack in the Box Curly Fries',
+        },
+        {
+            id: 14,
+            filling_name: 'jack_doubleultimatebacon',
+            filling_description: 'Jack in the Box Ultimate Bacon',
+        },
+        {
+            id: 15,
+            filling_name: 'jack_minitacos',
+            filling_description: 'Jack in the Box Tiny Tacos',
+        },
+        {
+            id: 16,
+            filling_name: 'jack_sourdoughjack',
+            filling_description: 'Jack in the Box Sourdough Jack',
+        },
+        {
+            id: 17,
+            filling_name: 'jack_spicychicken',
+            filling_description: 'Jack in the Box Spicy Chicken',
+        },
+        {
+            id: 18,
+            filling_name: 'mcdonalds_bigmac',
+            filling_description: 'McDonalds Big Mac',
+        },
+        {
+            id: 19,
+            filling_name: 'mcdonalds_crispychicken',
+            filling_description: 'McDonalds Crispy Chicken',
+        },
+        {
+            id: 20,
+            filling_name: 'mcdonalds_filetofish',
+            filling_description: 'McDonalds Filet o Fish',
+        },
+        {
+            id: 21,
+            filling_name: 'mcdonalds_mcdouble',
+            filling_description: 'McDonalds McDouble',
+        },
+        {
+            id: 22,
+            filling_name: 'mcdonalds_mcgriddle',
+            filling_description: 'McDonalds McGriddle',
+        },
+        {
+            id: 23,
+            filling_name: 'mcdonalds_mcmuffin',
+            filling_description: 'McDonalds McMuffin',
+        },
+        {
+            id: 24,
+            filling_name: 'mcdonalds_mcnuggets',
+            filling_description: 'McDonalds McNuggets',
+        },
+        {
+            id: 25,
+            filling_name: 'mcdonalds_mcrib',
+            filling_description: 'McDonalds McRib',
+        },
+        {
+            id: 26,
+            filling_name: 'wendys_baconator',
+            filling_description: 'Wendys Baconator',
+        },
+        {
+            id: 27,
+            filling_name: 'wendys_chili',
+            filling_description: 'Wendys Chili',
+        },
+        {
+            id: 28,
+            filling_name: 'wendys_davesdouble',
+            filling_description: 'Wendys Daves Double',
+        },
+        {
+            id: 29,
+            filling_name: 'wendys_spicychicken',
+            filling_description: 'Wendys Spicy Chicken',
+        }
+    ]
+  }
+
+
+  function makeItemsArray() {
+    return [
+        {
+            item_name: 'burgerking_baconking',
+            item_description: 'Burger King Bacon King',
+            bun_id: 1,
+            filling_id: 1,
+            price: 6.79,
+        },
+        {
+            item_name: 'burgerking_chickenfries',
+            item_description: 'Burger King Chicken Fries',
+            bun_id: null,
+            filling_id: 2,
+            price: ,
+        },
+        {
+            item_name: 'burgerking_doublecheeseburger',
+            item_description: 'Burger King Double Cheeseburger',
+            bun_id: 1,
+            filling_id: 3,
+            price: ,
+        },
+        {
+            item_name: 'burgerking_hersheyspie',
+            item_description: 'Burger King Hersheys Sundae Pie',
+            bun_id: null,
+            filling_id: 4,
+            price: ,
+        },
+        {
+            item_name: 'burgerking_whopper',
+            item_description: 'Burger King Whopper',
+            bun_id: 1,
+            filling_id: 5,
+            price: ,
+        },
+        {
+            item_name: 'carlsjr_doublewesternbacon',
+            item_description: 'Carls Jr Double Western Bacon',
+            bun_id: 2,
+            filling_id: 6,
+            price: ,
+        },
+        {
+            item_name: 'carlsjr_jalapenopopper',
+            item_description: 'Carls Jr Jalapeno Poppers',
+            bun_id: null,
+            filling_id: 7,
+            price: ,
+        },
+        {
+            item_name: 'carlsjr_superstar',
+            item_description: 'Carls Jr Super Star',
+            bun_id: 2,
+            filling_id: 8,
+            price: ,
+        },
+        {
+            item_name: 'innout_animalstylefries',
+            description: 'In N Out Animal Style Fries',
+            bun_id: null,
+            filling_id: 9,
+            price: ,
+        },
+        {
+            item_name: 'innout_doubledouble',
+            description: 'In N Out Double Double',
+            bun_id: 3,
+            filling_id: 10,
+            price: ,
+        },
+        {
+            item_name: 'innout_neopolitan',
+            item_description: 'In N Out Neopolitan Shake',
+            bun_id: null,
+            filling_id: 11,
+            price: ,
+        },
+        {
+            item_name: 'jack_baconswissbuttery',
+            item_description: 'Jack in the Box Bacon & Swiss Butter Jack',
+            bun_id: 5,
+            filling_id: 12,
+            price: ,
+        },
+        {
+            item_name: 'jack_curlyfries',
+            item_description: 'Jack in the Box Curly Fries',
+            bun_id: null,
+            filling_id: 13,
+            price: ,
+        },
+        {
+            item_name: 'jack_doubleultimatebacon',
+            item_description: 'Jack in the Box Ultimate Bacon',
+            bun_id: 4,
+            filling_id: 14,
+            price: ,
+        },
+        {
+            item_name: 'jack_minitacos',
+            item_description: 'Jack in the Box Tiny Tacos',
+            bun_id: null,
+            filling_id: 15,
+            price: ,
+        },
+        {
+            item_name: 'jack_sourdoughjack',
+            item_description: 'Jack in the Box Sourdough Jack',
+            bun_id: 6,
+            filling_id: 16,
+            price: ,
+        },
+        {
+            item_name: 'jack_spicychicken',
+            item_description: 'Jack in the Box Spicy Chicken',
+            bun_id: 4,
+            filling_id: 17,
+            price: ,
+        },
+        {
+            item_name: 'mcdonalds_bigmac',
+            item_description: 'McDonalds Big Mac',
+            bun_id: 10,
+            filling_id: 18,
+            price: ,
+        },
+        {
+            item_name: 'mcdonalds_crispychicken',
+            item_description: 'McDonalds Crispy Chicken',
+            bun_id: 7,
+            filling_id: 19,
+            price: ,
+        },
+        {
+            item_name: 'mcdonalds_filetofish',
+            item_description: 'McDonalds Filet o Fish',
+            bun_id: 11,
+            filling_id: 20,
+            price: ,
+        },
+        {
+            item_name: 'mcdonalds_mcdouble',
+            item_description: 'McDonalds McDouble',
+            bun_id: 11,
+            filling_id: 21,
+            price: ,
+        },
+        {
+            item_name: 'mcdonalds_mcgriddle',
+            item_description: 'McDonalds McGriddle',
+            bun_id: 8,
+            filling_id: 22,
+            price: ,
+        },
+        {
+            item_name: 'mcdonalds_mcmuffin',
+            item_description: 'McDonalds McMuffin',
+            bun_id: 9,
+            filling_id: 23,
+            price: ,
+        },
+        {
+            item_name: 'mcdonalds_mcnuggets',
+            item_description: 'McDonalds McNuggets',
+            bun_id: null,
+            filling_id: 24,
+            price: ,
+        },
+        {
+            item_name: 'mcdonalds_mcrib',
+            item_description: 'McDonalds McRib',
+            bun_id: null,
+            filling_id: 25,
+            price: ,
+        },
+        {
+            item_name: 'wendys_baconator',
+            item_description: 'Wendys Baconator',
+            bun_id: 12,
+            filling_id: 26,
+            price: ,
+        },
+        {
+            item_name: 'wendys_chili',
+            item_description: 'Wendys Chili',
+            bun_id: null,
+            filling_id: 27,
+            price: ,
+        },
+        {
+            item_name: 'wendys_davesdouble',
+            item_description: 'Wendys Daves Double',
+            bun_id: 12,
+            filling_id: 28,
+            price: ,
+        },
+        {
+            item_name: 'wendys_spicychicken',
+            item_description: 'Wendys Spicy Chicken',
+            bun_id: 12,
+            filling_id: 29,
+            price: ,
+        }
+    ]
+  }
+  }
+  
+  function makeCreationsArray() {
+    return [
+      {
+        id: 1,
+        question: 'Who is Eddie?',
+        answer: 'doesntreallymatter',
+        category: 'movies',
+        points: 200
+      },
+      {
+        id: 2,
+        question: 'Who is Donnie?',
+        answer: 'doesntreallymatter',
+        category: 'movies',
+        points: 200
+      },
+      {
+        id: 3,
+        question: 'Who is Greg?',
+        answer: 'doesntreallymatter',
+        category: 'science',
+        points: 200
+      },
+      {
+        id: 4,
+        question: 'Who is Scottie?',
+        answer: 'doesntreallymatter',
+        category: 'science',
+        points: 200
+      },
+    ]
+  }
+
+  
+  function makeFastFoodFixtures() {
+    const testBuns = makeBunsArray()
+    const testFillings = makeFillingsArray()
+    const testSauces = makeSaucesArray()
+    const testCreations = makeCreationsArray()
+    return { testBuns, testFillings, testSauces, testCreations }
+  }
+  
+  function cleanTables(db) {
+    return db.raw(
+      `TRUNCATE
+        fastfood_creations,
+        fastfood_items,
+        fastfood_fillings,
+        fastfood_buns,
+        fastfood_sauces
+        RESTART IDENTITY CASCADE`
+    )
+  }
+  
+  function seedUsers(db, users) {
+    const preppedUsers = users.map(user => ({
+      ...user,
+      password: bcrypt.hashSync(user.password, 1)
+    }))
+    return db.into('drivia_users').insert(preppedUsers)
+      .then(() =>
+        db.raw(
+          `SELECT setval('drivia_users_id_seq', ?)`,
+          [users[users.length -1 ].id],
+        )
+      )
+  }
+  
+  function seedQuestionsTables(db, users, questions) {
+    // use a transaction to group the queries and auto rollback on any failure
+    return db.transaction(async trx => {
+      await seedUsers(trx, users)
+      await trx.into('drivia_questions').insert(questions)
+      await trx.raw(
+               `SELECT setval('drivia_questions_id_seq', ?)`,
+               [questions[questions.length - 1].id],
+             )
+    })
+  }
+
+  function seedLeaderboardTable(db, users, scores) {
+    // use a transaction to group the queries and auto rollback on any failure
+    return db.transaction(async trx => {
+      await seedUsers(trx, users)
+      await trx.into('drivia_leaderboard').insert(scores)
+      await trx.raw(
+               `SELECT setval('drivia_leaderboard_id_seq', ?)`,
+               [scores[scores.length - 1].id],
+             )
+    })
+  }
+
+  function makeAuthHeader(user, secret = process.env.JWT_SECRET) {
+    const token = jwt.sign({ user_id: user.id }, secret, {
+      subject: user.user_name,
+      algorithm: 'HS256',
+    })
+    return `Bearer ${token}`
+  }
+  
+
+  
+  module.exports = {
+    makeUsersArray,
+    makeQuestionsArray,
+    makeExpectedScores,
+    makeLeaderboardArray,
+  
+    makeDriviaFixtures,
+    cleanTables,
+    seedQuestionsTables,
+    seedLeaderboardTable,
+    seedUsers,
+    makeAuthHeader
+  }
